6318a9f5cbeea32faa15d10f98873ba8138c2720
uluizeduardo <luiz.analistadesistemas@gmail.com>
2022-07-11 23:15:17 -0300

    UseState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -9,5 +9,5 @@ function NotificationButton() {
     )
   }
   
-  export default NotificationButton
+  export default NotificationButton;
   
